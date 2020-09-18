d1fb1e133242619e94eb181abe80717ca44b15cc
anickel101 <anson.nickel@gmail.com>
2020-09-17 20:51:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+const findMatching = (driverArray, driverName) => {
+    return driverArray.filter(driver => {
+        return driver.toLowerCase() === driverName.toLowerCase();
+    });
+}
+
+//.indexOf returns '-1' if value doesn't exist in string. Returns '0' if the partial begins at START.
+const fuzzyMatch = (driverArray, partialName) => {
+    return driverArray.filter(driver => {
+        return driver.toLowerCase().indexOf(partialName.toLowerCase()) === 0;
+    });
+
+}
+
+const matchName = (driverAofOs, driverName) => {
+    return driverAofOs.filter(driverObj => {
+        return driverObj.name === driverName;
+    })
+
+}
