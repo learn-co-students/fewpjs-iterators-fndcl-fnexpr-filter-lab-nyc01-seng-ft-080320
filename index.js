01346da028d1c6d4413af6526272809194b35606
Russell Sutter <russell.sutter@gmail.com>
2020-09-19 18:35:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+let findMatching = (drivers, string) => {
+  return drivers.filter( driver => driver.toLowerCase() === string.toLowerCase())
+}
+
+
+let fuzzyMatch = (drivers, string ) => {
+return drivers.filter( driver => driver[0].toLowerCase() === string[0].toLowerCase())
+}
+
+
+let matchName = (drivers, string ) => {
+  return drivers.filter( driver => driver.name === string )
+}
